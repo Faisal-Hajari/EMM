b0380ad98834047517c160259973a07288bc68fb
Faisal <faisalhejary@gmail.com>
2024-06-01 20:55:31 +0300

    error in function name for loader

--- data/__init__.py
+++ data/__init__.py
@@ -3,6 +3,6 @@ from .data_finetune import build_loader_finetune
 
 def build_loader(config, logger, is_pretrain):
     if is_pretrain:
-        return build_loader(config, logger)
+        return build_loader_pretrain(config, logger)
     else:
         return build_loader_finetune(config, logger)
