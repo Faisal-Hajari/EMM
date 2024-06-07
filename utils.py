f50f93a9dd5138f1092131da260eb9ee78446f43
Faisal <faisalhejary@gmail.com>
2024-06-08 01:31:42 +0300

    fixed some loading issue when finetuneing

--- utils.py
+++ utils.py
@@ -90,6 +90,9 @@ def load_pretrained(config, model, logger):
     checkpoint = torch.load(config["PRETRAINED"], map_location="cpu")
     checkpoint_model = checkpoint["model"]
     
+    if any([True if '_orig_mod.' in k else False for k in checkpoint_model.keys()]):
+        checkpoint_model = {k.replace('_orig_mod.', ""): v for k, v in checkpoint_model.items()}
+   
     if any([True if "encoder." in k else False for k in checkpoint_model.keys()]):
         checkpoint_model = {k.replace("encoder.", ""): v for k, v in checkpoint_model.items() if k.startswith("encoder.")}
         logger.info("Detect pre-trained model, remove [encoder.] prefix.")
@@ -198,11 +201,12 @@ def remap_pretrained_keys_vit(model, checkpoint_model, logger):
     # Duplicate shared rel_pos_bias to each layer
     if getattr(model, "use_rel_pos_bias", False) and "rel_pos_bias.relative_position_bias_table" in checkpoint_model:
         logger.info("Expand the shared relative position embedding to each transformer block.")
-    num_layers = model.get_num_layers()
-    rel_pos_bias = checkpoint_model["rel_pos_bias.relative_position_bias_table"]
-    for i in range(num_layers):
-        checkpoint_model["blocks.%d.attn.relative_position_bias_table" % i] = rel_pos_bias.clone()
-    checkpoint_model.pop("rel_pos_bias.relative_position_bias_table")
+    
+    # num_layers = model.get_num_layers()
+    # rel_pos_bias = checkpoint_model["rel_pos_bias.relative_position_bias_table"]
+    # for i in range(num_layers):
+    #     checkpoint_model["blocks.%d.attn.relative_position_bias_table" % i] = rel_pos_bias.clone()
+    # checkpoint_model.pop("rel_pos_bias.relative_position_bias_table")
     
     # Geometric interpolation when pre-trained patch size mismatch with fine-tuned patch size
     all_keys = list(checkpoint_model.keys())
